3f0b7260c19afec753ce8b9d98396d43fcdbd8c5
MarkKardash <152450137+MarkKardash@users.noreply.github.com>
2024-04-29 15:04:35 -0400

    Update saveLoad.java
    Please correct indentation if wrong. It is most likely wrong as I am very confused about it.

--- src/model/saveLoad.java
+++ src/model/saveLoad.java
@@ -4,19 +4,20 @@ import java.io.*;
 
 public class DataPersistenceManager {
 	  public static void saveCourses(Course course, String filename) {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
-          out.WriteObject(course);
-          } catch (IOException e) {
-              System.err.println("Error saving courses: " + e.getMessage());
-        }
-    }
+		  try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
+          		out.WriteObject(course);
+		  } catch (IOException e) {
+			  System.err.println("Error saving courses: " + e.getMessage());
+		  }
+	  }
+	
 	
 	public static Course course loadCourses(String filename) {
-		  try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
-        return (course) in.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.printin("Error loading courses: " + e.getMessage());
-            return null; 
-				}
-  }
+		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
+			return (course) in.readObject();
+			} catch (IOException | ClassNotFoundException e) {
+			    System.err.printin("Error loading courses: " + e.getMessage());
+			    return null; 
+		}
+	}
 }
